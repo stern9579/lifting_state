dc65ed81174bb8299b4e5ff7073c577f763cd7ed
Andrew Stern <andrewdstern16@gmail.com>
2022-03-15 12:22:52 -0600

    Enter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import MessageForm from './components/MessageForm';
+import MessageDisplay from './components/MessageDisplay';
 
+
+// imports removed for brevity
 function App() {
+  const [currentMsg, setCurrentMsg] = useState("There are no messages");
+
+  const youveGotMail = (newMessage) => {
+    setCurrentMsg(newMessage);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MessageForm onNewMessage={youveGotMail} />
+      <MessageDisplay message={currentMsg} />
+    </>
   );
 }
 
 export default App;
+
+
---
+++ src/components/MessageForm.jsx
@@ -0,0 +1,28 @@
+import react, { useState } from 'react';
+
+
+const MessageForm = (props) => {
+    const [msg, setMsg] = useState("");
+
+    // most of the component removed for brevity
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.onNewMessage(msg);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h1>Set Message</h1>
+            <textarea
+                rows="4"
+                cols="50"
+                placeholder="Enter your message here"
+                onChange={(e) => setMsg(e.target.value)}
+                value={msg}
+            ></textarea>
+            <input type="submit" value="Send Message" />
+        </form>
+    );
+};
+
+export default MessageForm;
